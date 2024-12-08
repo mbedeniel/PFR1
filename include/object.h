e3128ce61d92586e261a22cf66fe7400a15eafd1
Adjiaram <150181617+Adjiaram@users.noreply.github.com>
2024-12-08 17:32:10 +0100

    Delete include directory

--- include/object.h
+++
@@ -1,6 +0,0 @@
-/*un objet est defini par sa position, sa forme et sa couleur*/
-typedef struct {
-    Position position;
-    Shape shape;
-    Color color;
-} Object;
--- include/position_treatment.h
+++
@@ -1,5 +0,0 @@
-/* la structure Position qui contient les coordonnées x et y d'un objet*/
-typedef struct {
-    int x;
-    int y;
-} Position;

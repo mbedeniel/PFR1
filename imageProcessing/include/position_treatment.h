0c522126e75ea020f74722148c473d5ff70c2c13
mbedeniel <mbedeniel@gmail.com>
2024-12-23 01:26:54 +0100

    Cleaning

--- imageProcessing/include/position_treatment.h
+++
@@ -1,5 +0,0 @@
-/* la structure Position qui contient les coordonnées x et y d'un objet*/
-typedef struct {
-    int x; /*ligne occupée dans la matrice de pixel*/
-    int y; /*colonne occupée dans la matrice de pixel*/
-} Position;

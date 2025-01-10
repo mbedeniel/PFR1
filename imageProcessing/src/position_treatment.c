da9c72280a33691018212ba0fc832dc3625d1229
mbedeniel <mbedeniel@gmail.com>
2025-01-10 22:52:12 +0100

    update de position : ajout de commentaire dans position.c

--- imageProcessing/src/position_treatment.c
+++ imageProcessing/src/position_treatment.c
@@ -10,5 +10,11 @@ Position init_position()
 
 Position get_pixel_position(Position lowest_pixel)
 {
+    /*
+    Nous aurons pu ne pas mettre cette fonction
+    car elle n'effectue aucun calcul. Mais nous avons decidé 
+    de la mettre car nous utiliserons cette architecture au PFR2 
+    Et des calculs seront reelement realisées dans cette fonction
+    */
     return lowest_pixel;
 }

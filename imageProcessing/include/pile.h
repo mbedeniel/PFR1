47a8c99243b7d899b32c1ef61f679dd54db31741
wcm3415a <imran.wachill@univ-tlse3.fr>
2025-01-21 16:12:53 +0100

    Create pile.h
    pile

---
+++ imageProcessing/include/pile.h
@@ -0,0 +1,43 @@
+typedef int ELEMENT;
+
+void affiche_ELEMENT(ELEMENT element);
+
+typedef struct cell{
+    struct cell *suivant;
+    int x;
+    int y;
+} CELLULE;
+
+typedef CELLULE * PILE;
+
+
+PILE init_PILE(void);
+
+void affiche_PILE(PILE p);
+
+int PILE_estVide(PILE p);
+
+PILE emPILE(PILE p, int x, int y);
+
+int connexN(PILE p, int x, int y, int n);
+
+typedef struct cellu{
+    struct cellu *suivant;
+    PILE p;
+    int num_object;
+} CELLULEP;
+
+typedef CELLULEP * PILE_stack;
+
+PILE_stack init_PILE_stack(void);
+
+
+PILE_stack emPILE_stack(PILE_stack ps, PILE p);
+
+int PILE_estVide_stack(PILE_stack ps);
+
+PILE_stack parcour_PILE_connex(PILE_stack ps, int x, int y, int n);
+
+void objecBinariser(PILE_stack ps, int HEIGHT, int WIDTH, int CHANNELS, int seuil);
+
+void affiche_PILE_stack(PILE_stack ps);
